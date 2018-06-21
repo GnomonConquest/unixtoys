ca96b49f80f8c7433b31abe5bed6af9ebc148d7f
GnomonConquest <dimitry.dukhovny@gmail.com>
2018-06-20 21:01:26 -0700

    Update and rename ddstat.py to ddstats.py

--- ddstats.py
+++ ddstats.py
@@ -16,7 +16,13 @@ to do in the first place.
 @license:    GNU General Public License, version 3
 
 @contact:    dimitry <AT> dukhovny <DOT> net
-@deffield    updated: 20180619
+@deffield    updated: 20180620
+
+If TESTRUN=1 and you are running this as a script,
+ __name__ should be '__main__' as below...
+
+>>> __name__
+'__main__'
 '''
 
 import sys
@@ -28,9 +34,9 @@ from time import sleep
 from optparse import OptionParser
 
 __all__ = []
-__version__ = 0.3
-__date__ = '2018-06-19'
-__updated__ = '2018-06-19'
+__version__ = 0.4
+__date__ = '2018-06-20'
+__updated__ = '2018-06-20'
 
 DEBUG = 0
 TESTRUN = 0
@@ -41,19 +47,30 @@ WIN32 = False
 if os.name == 'nt':
     WIN32 = True
 
+
 def OUTPUT(outstring):
+    '''Sends text to stdout with a carriage return.
+    >>> OUTPUT('mystring')
+    mystring
+    '''
     outstring = str(outstring)
     sys.stdout.write(outstring + '\n')
 
 
 def OUTERR(outstring):
+    '''Sends text to stderr with a carriage return.  Stdout should get nothing.
+    >>> OUTERR('mystring')
+    '''
     outstring = str(outstring)
     sys.stderr.write(outstring + '\n')
 
 
 def sizeof_fmt(num, suffix='B'):
-    '''Human-printable value generator stolen from Fred Cirera in 2007.'''
-    for unit in ['','K','M','G','T','P','E','Z']:
+    '''Human-printable value generator stolen from Fred Cirera in 2007.
+    >>> sizeof_fmt(2893749120834)
+    '2.6TB'
+    '''
+    for unit in ['', 'K', 'M', 'G', 'T', 'P', 'E', 'Z']:
         if abs(num) < 1024.0:
             return "%3.1f%s%s" % (num, unit, suffix)
         num /= 1024.0
@@ -62,40 +79,58 @@ def sizeof_fmt(num, suffix='B'):
 
 def totalcpu():
     '''Returns a float percent of CPU utilization from a one-second
-    snapshot.'''
+    snapshot.
+    >>> type(totalcpu())
+    <type 'float'>
+    '''
     return(psutil.cpu_percent(1, False))
 
 
 def eachcpu():
-    '''Returns a list CPU utilization percentages for each CPU in order.'''
+    '''Returns a list CPU utilization percentages for each CPU in order.
+    >>> type(eachcpu())
+    <type 'list'>
+    '''
     return(psutil.cpu_percent(1, True))
 
 
 def ramused():
-    '''Returns a float percent of RAM utilization.'''
+    '''Returns a float percent of RAM utilization.
+    >>> type(ramused())
+    <type 'float'>
+    '''
     return(psutil.virtual_memory().percent)
 
 
 def ramratio():
-    '''Returns an array ['x', 'y'] of x memory in use and y total memory.'''
+    '''Returns an array ['x', 'y'] of x memory in use and y total memory.
+    >>> type(ramratio())
+    <type 'list'>
+    '''
     ram = psutil.virtual_memory()
     return([sizeof_fmt(ram.total - ram.available), sizeof_fmt(ram.total)])
 
 
 def totaldiskio(sample_interval=5):
     '''Takes a sample_interval in seconds and returns transactions per second.
-    This includes reads and writes from all disks.'''
+    This includes reads and writes from all disks.
+    >>> type(totaldiskio(0.01))
+    <type 'float'>
+    '''
     diskio1 = psutil.disk_io_counters(perdisk=False)
     sleep(sample_interval)
     diskio2 = psutil.disk_io_counters(perdisk=False)
-    t = (diskio2.read_count + diskio2.write_count - diskio1.read_count 
-         - diskio1.write_count)
+    t = (diskio2.read_count + diskio2.write_count - diskio1.read_count -
+         diskio1.write_count)
     return(float(t)/float(sample_interval))
 
 
 def eachdiskio(sample_interval=5):
     '''Returns a dictionary of { 'N': x } for each disk N and x transactions
-    per second.'''
+    per second.
+    >>> type(eachdiskio(0.01))
+    <type 'dict'>
+    '''
     outdict = {}
     diskio1 = psutil.disk_io_counters(perdisk=True)
     sleep(sample_interval)
@@ -109,35 +144,61 @@ def eachdiskio(sample_interval=5):
     return(outdict)
 
 
+def fetchdiskspace(partitionobject):
+    '''Gets the disk usage from an object returned by psutil.disk_partition().
+    It returns a key-value pair of the partition name and its percentage of use
+    or the last exception thrown in the attempt.
+    >>> type(fetchdiskspace(psutil.disk_partitions()[0]))
+    <type 'dict'>
+    '''
+    try:
+        k = partitionobject.device
+    except:
+        k = 'UnknownDevice'
+    try:
+        v = psutil.disk_usage(partitionobject.mountpoint).percent
+    except:
+        v = -1
+    return({k: v})
+
+
 def eachdiskspace():
     '''Returns a dictionary of { 'N': x } for each device N and x percent of
-    disk consumed, eliminating redundancies.'''
+    disk consumed, eliminating redundancies.
+    >>> type(eachdiskspace())
+    <type 'dict'>
+    '''
     diskdict = {}
-    map(lambda x: 
-        diskdict.update(
-            {x.device: psutil.disk_usage(x.mountpoint).percent}
-            ),
-         psutil.disk_partitions())
+    map(lambda x: diskdict.update(fetchdiskspace(x)), psutil.disk_partitions())
     return(diskdict)
 
 
 def processcount():
-    '''Returns number of running processes.'''
+    '''Returns number of running processes.
+    >>> type(processcount())
+    <type 'int'>
+    '''
     return(len(psutil.pids()))
 
 
 def uniqueprocesscount(processlist=[]):
-    '''Returns number of unique process names.'''
+    '''Returns number of unique process names.
+    >>> type(uniqueprocesscount())
+    <type 'int'>
+    '''
     if not processlist:
-        processlist = psutil.get_process_list()
+        processlist = psaux()
     unique_procs = len(set(map(lambda x: x.name(), processlist)))
     return(unique_procs)
 
 
 def fhcount(processlist=[]):
-    '''Returns number of file handles in use.'''
+    '''Returns number of unique process names.
+    >>> type(fhcount())
+    <type 'int'>
+    '''
     if not processlist:
-        processlist = psutil.get_process_list()
+        processlist = psaux()
     try:
         if WIN32:
             fhcounter = sum(map(lambda x: x.num_handles(), processlist))
@@ -148,41 +209,78 @@ def fhcount(processlist=[]):
     return(fhcounter)
 
 
+def psaux():
+    '''Get process list by either new or old means.  This is for backward
+    compatibility to old or <gasp>Win32</gasp> versions of psutil.
+    >>> type(psaux()[0])
+    <class 'psutil.Process'>
+    '''
+    try:
+        outlist = psutil.get_process_list()
+    except:
+        try:
+            outlist = list(psutil.process_iter())
+        except:
+            outlist = []
+    return(outlist)
+
+
 def main(argv=None):
-    '''Command line options.'''
+    '''Command line options parsing.  Takes arrays and returns an integers.
+    >>> main([])
+    0
+    '''
     program_name = os.path.basename(sys.argv[0])
     program_version = __version__
     program_build_date = "%s" % __updated__
 
-    program_version_string = '%%prog %s (%s)' % (program_version, program_build_date)
+    program_version_string = '%%prog %s (%s)' % (program_version,
+                                                 program_build_date)
     program_longdesc = '''Print stripped-down system stats.'''
-    program_license = "Copyright 2018 Dimitry Dukhovny Licensed under the GNU General Public License, version 3 https://www.gnu.org/licenses/gpl.txt"
+    program_license = '''Copyright 2018 Dimitry Dukhovny Licensed under the
+    GNU General Public License, version 3
+    https://www.gnu.org/licenses/gpl.txt'''
 
     if argv is None:
         argv = sys.argv[1:]
     try:
         # setup option parser
-        parser = OptionParser(version=program_version_string, epilog=program_longdesc, description=program_license)
-        parser.add_option("-v", "--verbose", dest="verbose", action="store_true",
-                          help="set verbosity level [default: %default]")
+        parser = OptionParser(version=program_version_string,
+                              epilog=program_longdesc,
+                              description=program_license)
+        parser.add_option("-v", "--verbose", dest="verbose",
+                          action="store_true",
+                          help="set verbosity level \
+                          [default: %default]")
         parser.add_option("-l", "--label", dest="label", action="store_true",
-                          help="add INI-style labels to output [default: %default]")
-        parser.add_option("-t", "--interval", dest="sample_interval", action="store", type="int",
-                          help="Set the interval for polling disk IO. [default: %default]")
+                          help="add INI-style labels to output \
+                          [default: %default]")
+        parser.add_option("-t", "--interval", dest="sample_interval",
+                          action="store", type="int",
+                          help="Set the interval for polling disk IO. \
+                          [default: %default]")
         parser.add_option("-u", "--cpu", dest="cpu", action="store_true",
-                          help="Get total CPU utilization or per-CPU if verbose")
+                          help="Get total CPU utilization \
+                          or per-CPU if verbose")
         parser.add_option("-m", "--memory", dest="memory", action="store_true",
-                          help="Get total memory utilization or ratio if verbose")
+                          help="Get total memory utilization \
+                          or ratio if verbose")
         parser.add_option("-d", "--disk", dest="disk", action="store_true",
                           help="Get per-device disk utilization")
         parser.add_option("-i", "--diskio", dest="diskio", action="store_true",
-                          help="Get total transactions per second for a specified interval or per-disk if verbose")
+                          help="Get total transactions per second \
+                          for a specified interval or per-disk if verbose")
         parser.add_option("-p", "--procs", dest="procs", action="store_true",
-                          help="Get process count or compare unique process count to total if verbose.")
-        parser.add_option("-f", "--filehandles", dest="handles", action="store_true",
-                          help="Get number of open file handles.  Usually requires privilege escalation.")
+                          help="Get process count \
+                          or compare unique process count \
+                          to total if verbose.")
+        parser.add_option("-f", "--filehandles", dest="handles",
+                          action="store_true",
+                          help="Get number of open file handles.  \
+                          Usually requires privilege escalation.")
         parser.add_option("-a", "--all", dest="all", action="store_true",
-                          help="Perform all of the above checks.  Usually requires privilege escalation.")
+                          help="Perform all of the above checks.  \
+                          Usually requires privilege escalation.")
 
         # set defaults
         parser.set_defaults(verbose=False)
@@ -198,10 +296,12 @@ def main(argv=None):
 
         # process options
         (opts, args) = parser.parse_args(argv)
+        if args:
+            sys.argv.append['-h']
 
         # reusables
         processlist = []
-        
+
         # Handling our flags
         if opts.cpu or opts.all:
             if opts.label:
@@ -237,7 +337,7 @@ def main(argv=None):
                 OUTERR('\n[PROCESSES]')
             if opts.verbose:
                 if not processlist:
-                    processlist = psutil.get_process_list()
+                    processlist = psaux()
                 OUTPUT(str(uniqueprocesscount(processlist)) +
                        ' unique from a total of ' + str(processcount()))
             else:
@@ -246,7 +346,7 @@ def main(argv=None):
             if opts.label:
                 OUTERR('\n[HANDLES]')
             if not processlist:
-                processlist = psutil.get_process_list()
+                processlist = psaux()
             OUTPUT(fhcount(processlist))
 
         # MAIN BODY #
@@ -258,13 +358,15 @@ def main(argv=None):
         print(traceback.format_exc())
         return 2
 
+    return 0
+
 
 if __name__ == "__main__":
     if DEBUG:
         sys.argv.append("-h")
     if TESTRUN:
         import doctest
-        doctest.testmod()
+        doctest.testmod(verbose=True)
     if PROFILE:
         import cProfile
         import pstats
